f5444ac013af6f52cb98f984488b291754d0fc9d
Teboho Mphure <tebohomphure@gmail.com>
2021-01-27 22:47:34 +0200

    Create ValidateSubsequence.cs

---
+++ AlgoExpert/ValidateSubsequence.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+public class Program 
+{
+	public static bool IsValidSubsequence(List<int> array, List<int> sequence) 
+  {
+		// Write your code here.
+		int index = 0;
+		int arraySize = 0;
+		List<bool> allFound = new List<bool>();
+		List<int> foundAtIndex = new List<int>();
+		
+		
+		for (int i = 0; i < sequence.Count; i++)
+		{
+				for (int j = i; j < array.Count; j++)
+				{
+						if ((i == j || j > i) && sequence[i] == array[j] && !foundAtIndex.Contains(j))
+						{
+								allFound.Add(true);
+								foundAtIndex.Add(j);
+								break;
+						}
+				}
+		}		
+		return allFound.Count == sequence.Count;
+	}
+}
